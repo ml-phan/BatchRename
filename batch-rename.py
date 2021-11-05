4212c5ec5c6339b98584b2a6f3370a3a3aad09a6
Phan <phanmanhlinh@yahoo.com>
2021-11-05 17:56:29 +0100

    Second Draft

--- batch-rename.py
+++ batch-rename.py
@@ -12,7 +12,13 @@ class MainWindow(qt.QMainWindow):
         self.statusBar().showMessage("Ready")
         self.setGeometry(600, 300, 600, 300)
 
-        #QTree to view directory
+        # Tree Widget
+        self.left_widget = qt.QWidget()
+        self.left_widget.setLayout(qt.QVBoxLayout())
+        self.right_widget = qt.QWidget()
+        self.right_widget.setLayout(qt.QHBoxLayout())
+
+        # QTree to view directory
         self.model = qt.QFileSystemModel()
         self.model.setRootPath("")
         self.tree = qt.QTreeView()
@@ -22,7 +28,26 @@ class MainWindow(qt.QMainWindow):
         self.tree.setSortingEnabled(True)
         self.tree.resize(640, 480)
         self.tree.setSelectionMode(qt.QAbstractItemView.MultiSelection)
-        central_widget.layout().addWidget(self.tree)
+
+        self.rename_button = qt.QPushButton("Batch rename", clicked=self.batch_rename)
+
+        # Add Widget to Central Widget
+        self.left_widget.layout().addWidget(self.tree)
+        self.right_widget.layout().addWidget(self.rename_button)
+        central_widget.layout().addWidget(self.left_widget)
+        central_widget.layout().addWidget(self.right_widget)
+
+    def batch_rename(self):
+        dialog = qt.QDialog()
+        dialog.setMinimumSize(600, 500)
+        dialog.setWindowTitle("Batch rename")
+        dialog.exec_()
+
+
+class RenameWindow(qt.QMainWindow):
+    def __init__(self):
+        super().__init__()
+
 
 app = qt.QApplication([])
 main_window = MainWindow()
