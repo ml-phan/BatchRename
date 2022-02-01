1dfd8f5fbeeec8bd15fd86a5e1d6b652234ac8e5
Phan <phanmanhlinh@yahoo.com>
2022-02-01 18:34:43 +0100

    Update 27.02.2022

--- gallery-dl-helper.py
+++ gallery-dl-helper.py
@@ -1,7 +1,8 @@
+import os
 import time
 import subprocess
 from subprocess import CREATE_NO_WINDOW
-
+import concurrent.futures
 
 from PyQt5 import QtCore
 import PyQt5.QtWidgets as qt
@@ -19,9 +20,9 @@ class MainWindow(qt.QMainWindow):
 
         self.link_label = qt.QLabel("Link")
         self.link_field = qt.QLineEdit()
-        self.link_field.returnPressed.connect(self.download_clicked)
+        # self.link_field.returnPressed.connect(self.download_clicked)
         self.link_field.setMinimumWidth(300)
-        self.download_button = qt.QPushButton("Download", clicked=self.download_clicked)
+        self.download_button = qt.QPushButton("Download", clicked=self.download_multi)
         self.download_list = qt.QListWidget()
         self.download_button.setMaximumWidth(100)
         self.download_list.setMinimumSize(400, 400)
@@ -43,13 +44,13 @@ class MainWindow(qt.QMainWindow):
         central_widget.layout().addWidget(self.delete_field)
         central_widget.layout().addWidget(self.delete_button)
 
-    def download_clicked(self):
-        link = self.link_field.text()
-        self.link_field.clear()
-        self.download_list.addItem(link)
-        self.statusBar().showMessage("Downloading " + link)
-        p = subprocess.Popen("gallery-dl " + link)
-        self.finished_list.addItem(link)
+    # def download_clicked(self):
+    #     link = self.link_field.text()
+    #     self.link_field.clear()
+    #     self.download_list.addItem(link)
+    #     self.statusBar().showMessage("Downloading " + link)
+    #     p = subprocess.Popen("gallery-dl " + link)
+    #     self.finished_list.addItem(link)
 
     def delete_link(self):
         link = self.delete_field.text()
@@ -60,6 +61,11 @@ class MainWindow(qt.QMainWindow):
             r = self.download_list.row(delete_item[0])
             self.download_list.takeItem(r)
             self.statusBar().showMessage(delete_item[0].__str__())
+
+    def download_multi(self):
+        link = self.link_field.text()
+        with concurrent.futures.ProcessPoolExecutor() as executor:
+            executor.submit(lambda: os.system("Test"))
 
 
 app = qt.QApplication([])
