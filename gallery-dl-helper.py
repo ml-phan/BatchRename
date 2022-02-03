30ac5d2602bfde84df67344b6997f4b0e4db7ce9
Phan <phanmanhlinh@yahoo.com>
2022-02-03 16:45:48 +0100

    QThread successfully implemented in gallery-dl-helper.py

--- gallery-dl-helper.py
+++ gallery-dl-helper.py
@@ -6,11 +6,25 @@ import concurrent.futures
 
 from PyQt5 import QtCore
 import PyQt5.QtWidgets as qt
+from PyQt5.QtCore import QObject, pyqtSignal, QThread
+
+
+class WorkerDownloadConsole(QThread):
+    link = ""
+
+    def __init__(self, link):
+        super().__init__()
+        self.link = link
+
+    def run(self):
+        os.system("gallery-dl " + self.link)
 
 
 class MainWindow(qt.QMainWindow):
     def __init__(self):
         super().__init__()
+        self.download_list = []
+        self.finished_list = []
         self.setWindowTitle("gallery-dl-helper")
         self.statusBar().showMessage("Ready")
         self.setGeometry(800, 200, 0, 0)
@@ -22,14 +36,14 @@ class MainWindow(qt.QMainWindow):
         self.link_field = qt.QLineEdit()
         # self.link_field.returnPressed.connect(self.download_clicked)
         self.link_field.setMinimumWidth(300)
-        self.download_button = qt.QPushButton("Download", clicked=self.download_multi)
-        self.download_list = qt.QListWidget()
+        self.download_button = qt.QPushButton("Download", clicked=self.worker_download)
+        self.download_list_box = qt.QListWidget()
         self.download_button.setMaximumWidth(100)
-        self.download_list.setMinimumSize(400, 400)
+        self.download_list_box.setMinimumSize(400, 400)
         self.delete_button = qt.QPushButton("Delete", clicked=self.delete_link)
         self.delete_field = qt.QLineEdit()
-        self.finished_list = qt.QListWidget()
-        self.finished_list.setMinimumSize(400, 400)
+        self.finished_list_box = qt.QListWidget()
+        self.finished_list_box.setMinimumSize(400, 400)
         self.download_list_label = qt.QLabel("Downloading")
         self.finished_list_label = qt.QLabel("Finished")
 
@@ -38,34 +52,58 @@ class MainWindow(qt.QMainWindow):
         central_widget.layout().addWidget(self.download_button, 0, 0)
 
         central_widget.layout().addWidget(self.download_list_label, 1, 0)
-        central_widget.layout().addWidget(self.download_list, 2, 0, 1, 3)
+        central_widget.layout().addWidget(self.download_list_box, 2, 0, 1, 3)
         central_widget.layout().addWidget(self.finished_list_label, 3, 0)
-        central_widget.layout().addWidget(self.finished_list, 4, 0, 1, 3)
+        central_widget.layout().addWidget(self.finished_list_box, 4, 0, 1, 3)
         central_widget.layout().addWidget(self.delete_field)
         central_widget.layout().addWidget(self.delete_button)
 
-    # def download_clicked(self):
-    #     link = self.link_field.text()
-    #     self.link_field.clear()
-    #     self.download_list.addItem(link)
-    #     self.statusBar().showMessage("Downloading " + link)
-    #     p = subprocess.Popen("gallery-dl " + link)
-    #     self.finished_list.addItem(link)
+    def download_console(self):
+        link = self.link_field.text()
+        self.link_field.clear()
+        self.statusBar().showMessage("Downloading " + link)
+        self.download_list.append(link)
+        p = subprocess.call("gallery-dl " + link)
+        self.download_to_finished(link)
+        self.update_finished_list()
+
+    def update_download_list(self):
+        self.download_list_box.clear()
+        self.download_list_box.addItems(self.download_list)
+
+    def update_finished_list(self):
+        self.finished_list_box.clear()
+        self.finished_list_box.addItems(self.finished_list)
+
+    def download_to_finished(self, link):
+        self.download_list.remove(link)
+        self.finished_list.append(link)
 
     def delete_link(self):
         link = self.delete_field.text()
-        delete_item = self.download_list.findItems(link, QtCore.Qt.MatchExactly)
+        delete_item = self.download_list_box.findItems(link, QtCore.Qt.MatchExactly)
         if len(delete_item) == 0:
             self.statusBar().showMessage(link + " Not found")
         else:
-            r = self.download_list.row(delete_item[0])
-            self.download_list.takeItem(r)
+            r = self.download_list_box.row(delete_item[0])
+            self.download_list_box.takeItem(r)
             self.statusBar().showMessage(delete_item[0].__str__())
 
-    def download_multi(self):
+    def worker_download(self):
         link = self.link_field.text()
-        with concurrent.futures.ProcessPoolExecutor() as executor:
-            executor.submit(lambda: os.system("Test"))
+        self.link_field.clear()
+        self.statusBar().showMessage("Downloading " + link)
+        self.download_list.append(link)
+        self.update_download_list()
+        self.worker = WorkerDownloadConsole(link)
+        self.worker.start()
+        self.worker.finished.connect(lambda: self.download_worker_finished(link))
+
+    def download_worker_finished(self, link):
+        lnk = link
+        self.download_to_finished(lnk)
+        self.update_download_list()
+        self.update_finished_list()
 
 
 app = qt.QApplication([])
